5b628268b010fe48ab49779dd6e9d5308c2fa68c
sepryandayon810-debug <sepryandayon810@gmail.com>
2026-04-01 17:36:39 +0700

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,28 @@
 const Cart = {
   data: [],
 
+  init() {
+    this.load();
+  },
+
+  simpan() {
+    localStorage.setItem("cart", JSON.stringify(this.data));
+  },
+
+  load() {
+    const data = localStorage.getItem("cart");
+    this.data = data ? JSON.parse(data) : [];
+  },
+
   tambah(item) {
     this.data.push(item);
+    this.simpan();
     UI.renderCart();
   },
 
   clear() {
     this.data = [];
+    this.simpan();
     UI.renderCart();
   },
 
