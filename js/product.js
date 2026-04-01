0ce1b8d1564d8b38d2a39713788955c21412d5dc
sepryandayon810-debug <sepryandayon810@gmail.com>
2026-04-01 17:36:19 +0700

    Update product.js

--- js/product.js
+++ js/product.js
@@ -1,14 +1,29 @@
 const Product = {
   data: [],
 
+  init() {
+    this.load();
+  },
+
+  simpan() {
+    localStorage.setItem("produk", JSON.stringify(this.data));
+  },
+
+  load() {
+    const data = localStorage.getItem("produk");
+    this.data = data ? JSON.parse(data) : [];
+  },
+
   tambah(nama, harga) {
     this.data.push({ nama, harga: Number(harga) });
+    this.simpan();
     UI.renderProduk();
     UI.renderProdukKasir();
   },
 
   hapus(index) {
     this.data.splice(index, 1);
+    this.simpan();
     UI.renderProduk();
     UI.renderProdukKasir();
   },
